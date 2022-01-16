35539c8f98557689acf88f84a1934884d3c8745a
Lerqiu <mcwk810@gmail.com>
2022-01-16 17:03:04 +0100

    Adding bioms to

--- drafts/island_moisture.py
+++ drafts/island_moisture.py
@@ -10,3 +10,12 @@ def clearAt(array:np.ndarray,filterValue:Number):
 def joinOnMax(array1,array2):
     com = np.dstack((array1, array2))
     return np.max(com,axis=2)
+
+def normalize(array):
+    arrayMax = np.max(array)
+    if arrayMax > 1.0:
+        array = array/arrayMax
+    arrayMin = np.min(array)
+    if arrayMin < -1.0:
+        array = array/abs(arrayMin)
+    return array
